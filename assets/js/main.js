449dcf86fe228033a08c7a63567dcc659a1eff7a
angelacaterina <angela.caterina@icloud.com>
2020-11-24 15:45:21 +0100

    completamento programma campo minato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,59 @@
-//  1. Il computer deve generare 16 numeri casuali tra 1 e 100.
-// 2. I numeri non possono essere duplicati
-var listRndPc = [];
-while (listRndPc.length < 16){
-  var numRandomPc = Math.floor(Math.random() * 100) + 1 ;
+//// FUNZIONE: generatore di numeri casuali
 
-  if(listRndPc.includes(numRandomPc) == false){
-    listRndPc.push(numRandomPc);
+function getRandomNumber(min, max){
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+// console.log(getRandomNumber(1, 100));
+
+// 1. Il computer deve generare 16 numeri casuali tra 1 e 100.
+// 2. I numeri non possono essere duplicati
+var numbersPc = [];
+while (numbersPc.length !== 16){
+  var numRndPc = getRandomNumber(1, 100);
+  if(! inArray(numbersPc, numRndPc)){
+    numbersPc.push(numRndPc);
+  }
+
+  // if(numbersPc.includes(numRndPc) == false){
+  //   numbersPc.push(numRndPc);
+  // }
+}
+console.log(numbersPc, numbersPc.length);
+
+//// FUNZIONE: verifica se un numero è presente in array
+function inArray(array, number){
+  var i = 0;
+  while(i < array.length){
+    if(number === array[i]){
+      return true;
+    }
+    i++;
   }
 }
-
-console.log(listRndPc);
 
 // 3. in seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // 4. L’utente non può inserire più volte lo stesso numero.
 // 5. Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // 6. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-var listUser = [];
-while(listUser.length < 84){
+var numbersUser = [];
+var possibilita = 100-16;
+
+for(var i = 0; i < possibilita; i++){
   var userNumber = Number(prompt("insert a number from 1 to 100"));
-
-  if(listUser.includes(userNumber) == false){
-    listUser.push(userNumber);
-  }
-
-  if(listRndPc.includes(userNumber)){
-    console.log("hai perso");
+  if(inArray(numbersPc, userNumber)){
+    console.log("Bomba Game Over");
+    alert("Game Over, il tuo punteggio è: " + numbersUser.length );
     break;
   }
-
-  console.log(userNumber);
+  while(inArray(numbersUser,userNumber)){
+    alert("Hai già usato questo numero! Non puoi usare lo stesso numero più volte. Premi OK e riprova");
+    userNumber = Number(prompt("insert a number from 1 to 100"));
+  }
+  numbersUser.push(userNumber);
 }
-console.log(listUser);
+console.log(userNumber, numbersUser);
 
 // 7. Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-if (listUser.length == 84){
-  alert("punteggio: " + listUser.length);
+if (numbersUser.length == possibilita){
+  alert("Victory il tuo punteggio è: " + numbersUser.length);
 }
